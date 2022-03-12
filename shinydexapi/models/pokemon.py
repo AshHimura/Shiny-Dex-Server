d17182ea0408bacfbdbc5f2c99644baf5ac17eeb
Chris Mccrary <chris.mccrary@asurion.com>
2022-03-11 21:58:53 -0600

    created first fixture

--- shinydexapi/models/pokemon.py
+++ shinydexapi/models/pokemon.py
@@ -4,9 +4,10 @@ class Pokemon(models.Model):
     name = models.CharField(max_length=20)
     pokemonKind = models.CharField(max_length=40)
     description = models.TextField()
-    height = models.FloatField()
-    weight = models.FloatField()
-    items = models.ForeignKey("Item", on_delete=models.CASCADE)
+    standardHeight = models.CharField(max_length=10)
+    standardAlphaHeight = models.CharField(max_length=10)
+    standardWeight = models.FloatField()
+    standardAlphaWeight = models.FloatField()
     isShiny = models.BooleanField(default=False)
     isAlpha = models.BooleanField(default=False)
     
